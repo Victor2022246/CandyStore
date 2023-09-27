d38bd1085d42da8ebb097e319a924c6a41e19268
Victor2022246 <2022246@student.cct.ie>
2023-09-27 15:13:32 +0100

    Sweetclass
    create a Sweet abstract Class

---
+++ src/candystore/Sweets.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package candystore;
+
+/**
+ *
+ * @author victor pc
+ */
+public class Sweets {
+    String flavor;
+    String name;
+    String brand;
+
+    public Sweets(String flavor, String name, String brand) {
+        this.flavor = flavor;
+        this.name = name;
+        this.brand = brand;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+    
+    
+    
+    
+    
+    
+}
