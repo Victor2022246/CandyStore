8d166f446f6f43d46d066b89d7b53dc05ea9b81b
Victor2022246 <2022246@student.cct.ie>
2023-09-30 23:45:21 +0100

    create Chocolate clas
    new class creation

---
+++ src/candystore/Chocolate.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package candystore;
+
+/**
+ *
+ * @author victor pc
+ */
+public class Chocolate extends Sweets{
+    
+    public Chocolate(String flavor, String name, String brand) {
+        super(flavor, name, brand);
+    }
+    
+}
